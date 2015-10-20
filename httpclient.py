295799e9634b54d9837bba1428d65ec408bd0049
alecrobins <alecrobins@gmail.com>
2015-10-20 02:54:52 +0000

    changed port

--- httpclient.py
+++ httpclient.py
@@ -47,7 +47,7 @@ def main ():
         print "Exception thrown: ", sys.exc_info()[0]
         raise
 
-r = requests.get("http://localhost:8888/getVMS")
+r = requests.get("http://localhost:8080/getVMS")
 vmInfo = json.loads(r.text)
 
 vm1Name = vmInfo["VM1"]["name"]
@@ -61,37 +61,37 @@ if  i < 1000:
     start = default_timer()
 
     #Get load....
-    r2 = requests.get("http://localhost:8888/getLoad")
-    #vmLoads = json.loads(r2.text);
-    #print(vmLoads)
-#    load1 = vmLoads["VM1"]["load"]
-#    load2 = vmLoads["VM2"]["load"]
+    r2 = requests.get("http://localhost:8080/getLoad")
+    vmLoads = json.loads(r2.text);
+    print(vmLoads)
+    load1 = vmLoads["VM1"]["load"]
+    load2 = vmLoads["VM2"]["load"]
 
     
-    testSend1 = {}
-    testSend1['name'] = "testvmsmaller"
-    testSend1['load'] = .5
+    #testSend1 = {}
+    #testSend1['name'] = "testvmsmaller"
+    #testSend1['load'] = .5
     
-    testSend2 = {}
-    testSend2['name'] = "testvmbigger"
-    testSend2['load'] = .7
+    #testSend2 = {}
+    #testSend2['name'] = "testvmbigger"
+    #testSend2['load'] = .7
     
-    l1test = testSend1["load"]
-    l2test = testSend2["load"]
+    #l1test = testSend1["load"]
+    #l2test = testSend2["load"]
     
-    if l1test < l2test:
-        print("It works")
-        print(l1test)
-        print(l2test)
+    #if l1test < l2test:
+    #    print("It works")
+    #    print(l1test)
+    #    print(l2test)
 
     #Post
     if load1 < load2:
-        r3 = requests.post("http://localhost:8888/checkForPrimes", send1)
-        print("Is " + number + " a prime?")
+        r3 = requests.post("http://localhost:8080/checkForPrimes", i, vm1Name)
+        print("Is " + i + " a prime?")
         print(r3.text)
     else:
-        r4 = requests.post("http://localhost:8888/checkForPrimes", send2)
-        print("Is " + number + " a prime?")
+        r4 = requests.post("http://localhost:8080/checkForPrimes", i, vm2Name)
+        print("Is " + i + " a prime?")
         print(r4.text)
 
     duration = default_timer() - start
